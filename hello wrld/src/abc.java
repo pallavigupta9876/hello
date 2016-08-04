418f7da7cd3585876ed0970af1c3e6a1bdb98ebc
pallavi.gupta <pallavi.gupta@NST-PALLAVIGUPTA.svam.com>
2016-08-04 14:48:50 +0530

    Coommit

--- hello wrld/src/abc.java
+++ hello wrld/src/abc.java
@@ -2,9 +2,15 @@
 public class abc {
 	public static void main(String args[])
 	{
+		String[] a={"qwerty","mnbvc","lkjhg"};
 		
 		System.out.print("hello");
 		System.out.print("World");
+		
+		for(int i=0 ; i<a.length;i++)
+		{
+		System.out.println("Hello  "  +a[i]);	
+		}
 	}
 
 }
